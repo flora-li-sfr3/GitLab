91f86cf0536b797e1d29d490db4b132e6762f0d8
Flora Li <fli2@scu.edu>
2025-02-10 10:13:14 -0800

    description

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main () {
-  cout << "        Hello Wo               " << endl;
+  cout << "        Hello W               " << endl;
 
   MyClass a(5);
   cout << a.getX() << endl;
